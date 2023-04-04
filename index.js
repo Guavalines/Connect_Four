b22a7b732a17111688952789481464fb70236cf2
guavalines <konstrictor@gmail.com>
2023-04-03 22:32:57 -0700

    tested onclick functionality on all squares

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () =>
 {
   const squares = document.querySelectorAll('.grid div')
+  const result = document.querySelector('#result')
+  const displayCurrentPlayer = document.querySelector('#current-player')
+  let currentPlayer = 1
 
-
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+      alert('You have clicked square ' + i)
+    }
+  }
 
 })
