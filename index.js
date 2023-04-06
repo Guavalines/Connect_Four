7c37a5cf88aa4de98fd3500d2515a59b9a4fec04
guavalines <konstrictor@gmail.com>
2023-04-05 22:31:58 -0700

    fixed 2nd bug

--- index.js
+++ index.js
@@ -110,12 +110,17 @@ document.addEventListener('DOMContentLoaded', () =>
 
   for (let i = 0; i < squares.length; i++) {
     squares[i].onclick = () => {
-      //if the square below your current square is taken, you can go on top of it
-      if (squares[i + 7].classList.contains('taken')) &&!squares[i].classList.contains('taken')) {
+      //if the square below your current square is taken, you can go ontop of it
+      if (squares[i + 7].classList.contains('taken') &&!squares[i].classList.contains('taken')) {
         if (currentPlayer == 1) {
           squares[i].classList.add('taken')
           squares[i].classList.add('player-one')
           currentPlayer = 2
+          displayCurrentPlayer.innerHTML = currentPlayer
+        } else if (currentPlayer == 2){
+          squares[i].classList.add('taken')
+          squares[i].classList.add('player-two')
+          currentPlayer = 1
           displayCurrentPlayer.innerHTML = currentPlayer
         }
       } else alert('cant go here')
