d0591814a318ff9eab1a0245447ad9a6e5cd2997
guavalines <konstrictor@gmail.com>
2023-04-05 22:29:09 -0700

    fixed bug

--- index.js
+++ index.js
@@ -83,26 +83,27 @@ document.addEventListener('DOMContentLoaded', () =>
       const square2 = squares[winningArrays[y][1]]
       const square3 = squares[winningArrays[y][2]]
       const square4 = squares[winningArrays[y][3]]
-    }
-    //check those squares to see if they all have the class of player-one
-    if (
-      square1.classList.contains('player-one') &&
-      square2.classList.contains('player-one') &&
-      square3.classList.contains('player-one') &&
-      square4.classList.contains('player-one')
-    )
-    {
-      result.innerHTML = 'Player One Wins!'
-    }
-    //check those squares to see if they all have the class of player-two
-    if (
-      square1.classList.contains('player-two') &&
-      square2.classList.contains('player-two') &&
-      square3.classList.contains('player-two') &&
-      square4.classList.contains('player-two')
-    )
-    {
-      result.innerHTML = 'Player Two Wins!'
+
+      //check those squares to see if they all have the class of player-one
+      if (
+        square1.classList.contains('player-one') &&
+        square2.classList.contains('player-one') &&
+        square3.classList.contains('player-one') &&
+        square4.classList.contains('player-one')
+      )
+      {
+        result.innerHTML = 'Player One Wins!'
+      }
+      //check those squares to see if they all have the class of player-two
+      if (
+        square1.classList.contains('player-two') &&
+        square2.classList.contains('player-two') &&
+        square3.classList.contains('player-two') &&
+        square4.classList.contains('player-two')
+      )
+      {
+        result.innerHTML = 'Player Two Wins!'
+      }
     }
   }
 
@@ -110,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () =>
   for (let i = 0; i < squares.length; i++) {
     squares[i].onclick = () => {
       //if the square below your current square is taken, you can go on top of it
-      if (squares[i + 7].classList.contains('taken')) {
+      if (squares[i + 7].classList.contains('taken')) &&!squares[i].classList.contains('taken')) {
         if (currentPlayer == 1) {
           squares[i].classList.add('taken')
           squares[i].classList.add('player-one')
